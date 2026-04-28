52862b5551dab2f831052eeaaad12d52985fa7b1
ZahirFelixx <141684361+ZahirFelixx@users.noreply.github.com>
2026-04-28 13:10:22 -0700

    Revert "Feat/rf 18 info alum"

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// 'login' (es un estándar en Laravel)
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-
-// Ruta básica para el dashboard
-Route::get('/dashboard', function () {
-    return view('welcome'); // Por ahora lo mandamos a welcome, luego se debera actualizar por la vista real
-})->name('dashboard');
-
-Route::get('/grupos/importar', function () {
-    return view('groups.importar_alumnos');
-})->name('grupos.importar');
-
-Route::get('/asistencias/importar', function () {
-    return view('attendance.importar_asistencias');
-})->name('asistencias.importar');
